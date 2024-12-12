26f4e0111c17c488b71c7c97af554cf713c0613b
Claudiu Szidelo <claudiuszidelo@yahoo.ro>
2024-12-12 20:31:32 +0200

    fixed eventlistiner so it will trigger only when click on the button

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const sketchDisplay = document.querySelector("#sketch");
 
+const draw = (sq) => {
+	sq.style.backgroundColor = "#137e3e";
+};
+
 const drawSquares = (amount) => {
 	if (amount === 16) {
 		for (let i = 0; i < amount ** 2; i++) {
 			const square = document.createElement("div");
+			square.classList.add("square");
 			square.classList.add("square-lg");
+			square.setAttribute("draggable", "false");
 			sketchDisplay.appendChild(square);
+
+			square.addEventListener("mousedown", () => {
+				draw(square);
+			});
 		}
 	}
 };
 
 const btn16 = document.querySelector("#s16");
-btn16.addEventListener("click", drawSquares(16));
+btn16.addEventListener("click", (e) => {
+	drawSquares(16);
+});
