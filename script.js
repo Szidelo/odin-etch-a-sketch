9917148b4dd3eb0de17fce86bd445dcfb526da44
Claudiu Szidelo <claudiuszidelo@yahoo.ro>
2024-12-15 17:10:49 +0200

    changed variables name to be more descriptive

--- script.js
+++ script.js
@@ -8,70 +8,75 @@
 // on clear should reset the app
 // should keep the square classes in a list or constant to be able to change easyer
 
-const OPTIONS = {
-  SIXTEEN: 16,
-  THIRTYTWO: 32,
-  SIXTYFOUR: 64,
-  ONETWENTYEIGHT: 128,
+const GRID_SIZES = {
+    SMALL: 16,
+    MEDIUM: 32,
+    LARGE: 64,
+    EXTRA_LARGE: 128,
 };
 
-const SQUARE_CLASSES = {
-  SMALL: "square-sm",
-  MEDIUM: "square-md",
-  LARGE: "square-lg",
-  EXTRA_LARGE: "square-xl",
+const SQUARE_STYLES = {
+    XL_CLASS: "square-xl",
+    LARGE_CLASS: "square-lg",
+    MEDIUM_CLASS: "square-md",
+    SMALL_CLASS: "square-sm",
 };
 
-const sketchDisplay = document.querySelector("#sketch");
-const btnOptionSixteen = document.querySelector("#s16");
-const btnOptionThirthyTwo = document.querySelector("#s32");
-const btnOptionSixtyFour = document.querySelector("#s64");
-const btnOptionOneTwentyEight = document.querySelector("#s128");
+const sketchContainer = document.querySelector("#sketch");
+const btnSmallGrid = document.querySelector("#s16");
+const btnMediumGrid = document.querySelector("#s32");
+const btnLargeGrid = document.querySelector("#s64");
+const btnExtraLargeGrid = document.querySelector("#s128");
 
-const draw = (sq) => {
-  sq.style.backgroundColor = "#137e3e";
+const paintSquare = (squareElement) => {
+    squareElement.style.backgroundColor = "#137e3e";
 };
 
-const drawSquares = (amount) => {
-  const { SIXTEEN, THIRTYTWO, SIXTYFOUR, ONETWENTYEIGHT } = OPTIONS;
-  const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = SQUARE_CLASSES;
-  let cssClass;
-  switch (amount) {
-    case SIXTEEN:
-      cssClass = EXTRA_LARGE;
-      break;
-    case THIRTYTWO:
-      cssClass = LARGE;
-      break;
-    case SIXTYFOUR:
-      cssClass = MEDIUM;
-      break;
-    case ONETWENTYEIGHT:
-      cssClass = SMALL;
-      break;
-  }
+const generateGrid = (gridSize) => {
+    const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = GRID_SIZES;
+    const { SMALL_CLASS, MEDIUM_CLASS, LARGE_CLASS, XL_CLASS } = SQUARE_STYLES;
 
-  for (let i = 0; i < amount ** 2; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.classList.add(cssClass);
-    square.setAttribute("draggable", "false");
-    sketchDisplay.appendChild(square);
+    let squareClass;
+    switch (gridSize) {
+        case SMALL:
+            squareClass = XL_CLASS;
+            break;
+        case MEDIUM:
+            squareClass = LARGE_CLASS;
+            break;
+        case LARGE:
+            squareClass = MEDIUM_CLASS;
+            break;
+        case EXTRA_LARGE:
+            squareClass = SMALL_CLASS;
+            break;
+    }
 
-    square.addEventListener("mousedown", () => {
-      draw(square);
-    });
-  }
+    // clear existing squares before generating a new grid
+    sketchContainer.innerHTML = "";
+
+    // generate squares
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const squareElement = document.createElement("div");
+        squareElement.classList.add("square");
+        squareElement.classList.add(squareClass);
+        squareElement.setAttribute("draggable", "false");
+        sketchContainer.appendChild(squareElement);
+
+        // event listener for painting squares
+        squareElement.addEventListener("mousedown", () => {
+            paintSquare(squareElement);
+        });
+    }
 };
 
-btnOptionSixteen.addEventListener("click", (e) => {
-  console.log(e.target);
-  drawSquares(OPTIONS.SIXTEEN);
-});
+// Event listeners for grid size buttons
+btnSmallGrid.addEventListener("click", () => generateGrid(GRID_SIZES.SMALL));
+btnMediumGrid.addEventListener("click", () => generateGrid(GRID_SIZES.MEDIUM));
+btnLargeGrid.addEventListener("click", () => generateGrid(GRID_SIZES.LARGE));
+btnExtraLargeGrid.addEventListener("click", () =>
+    generateGrid(GRID_SIZES.EXTRA_LARGE)
+);
 
-btnOptionThirthyTwo.addEventListener("click", (e) => {
-  console.log(e.target);
-  drawSquares(OPTIONS.THIRTYTWO);
-});
-
-drawSquares(OPTIONS.SIXTEEN);
+// init default grid
+generateGrid(GRID_SIZES.SMALL);
