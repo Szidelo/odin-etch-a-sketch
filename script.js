863140850dae287dcfe9794ff38f540bcf5c9f6c
Szidelo <claudiuszidelo@yahoo.ro>
2024-12-20 16:50:23 +0200

    can paint multiple squares by holding mouse down and drag over

--- script.js
+++ script.js
@@ -7,105 +7,120 @@
 // when clicking the erase btn the square selected should reset to transparent background
 // on clear should reset the app
 // should keep the square classes in a list or constant to be able to change easyer
+// should be able to change the color of the square while holding mouse down
+// when changing the grid size the led light should turn on and off only for the selected size
 
 const COLORS = {
-    SQUARE_GREEN: "#137e3e",
-    SQUARE_TRANSPARENT: "transparent",
-    SQUARE_BORDER: "#137e3e",
+	SQUARE_GREEN: "#137e3e",
+	SQUARE_TRANSPARENT: "transparent",
+	SQUARE_BORDER: "#137e3e",
 };
 
 const GRID_SIZES = {
-    SMALL: 16,
-    MEDIUM: 32,
-    LARGE: 64,
-    EXTRA_LARGE: 128,
+	SMALL: 16,
+	MEDIUM: 32,
+	LARGE: 64,
+	EXTRA_LARGE: 128,
 };
 
 const SQUARE_STYLES = {
-    XL_CLASS: "square-xl",
-    LARGE_CLASS: "square-lg",
-    MEDIUM_CLASS: "square-md",
-    SMALL_CLASS: "square-sm",
+	XL_CLASS: "square-xl",
+	LARGE_CLASS: "square-lg",
+	MEDIUM_CLASS: "square-md",
+	SMALL_CLASS: "square-sm",
 };
 
 const prompts = document.querySelectorAll(".line");
 const display = document.querySelector("#display");
 const sketchContainer = document.createElement("div");
-sketchContainer.id = "sketch";
-console.log(sketchContainer);
 const optionButtons = document.querySelectorAll(".options");
 const toggleGridBtn = document.querySelector("#toggle-grid");
-
 const btnErase = document.querySelector("#eraser");
+
+sketchContainer.id = "sketch";
 
 let isEraseActive = false;
 let gameIsRunning = false;
 let gridIsVisible = true;
 
 const toggleGrid = () => {
-    gridIsVisible = !gridIsVisible;
-    const { SQUARE_TRANSPARENT, SQUARE_BORDER } = COLORS;
-    const squares = document.querySelectorAll(".square") || null;
-    if (squares) {
-        squares.forEach((square) => {
-            square.style.borderColor = gridIsVisible
-                ? SQUARE_BORDER
-                : SQUARE_TRANSPARENT;
-        });
-    }
+	gridIsVisible = !gridIsVisible;
+	const { SQUARE_TRANSPARENT, SQUARE_BORDER } = COLORS;
+	const squares = document.querySelectorAll(".square") || null;
+	if (squares) {
+		squares.forEach((square) => {
+			square.style.borderColor = gridIsVisible ? SQUARE_BORDER : SQUARE_TRANSPARENT;
+		});
+	}
 };
 
 const eraseSquare = (squareElement) => {
-    console.log("erase");
-    squareElement.style.backgroundColor = COLORS.SQUARE_TRANSPARENT;
+	squareElement.style.backgroundColor = COLORS.SQUARE_TRANSPARENT;
 };
 
 const paintSquare = (squareElement) => {
-    squareElement.style.backgroundColor = COLORS.SQUARE_GREEN;
+	squareElement.style.backgroundColor = COLORS.SQUARE_GREEN;
 };
 
 const generateGrid = (gridSize) => {
-    const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = GRID_SIZES;
-    const { SMALL_CLASS, MEDIUM_CLASS, LARGE_CLASS, XL_CLASS } = SQUARE_STYLES;
+	const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = GRID_SIZES;
+	const { SMALL_CLASS, MEDIUM_CLASS, LARGE_CLASS, XL_CLASS } = SQUARE_STYLES;
 
-    let squareClass;
-    switch (gridSize) {
-        case SMALL:
-            squareClass = XL_CLASS;
-            break;
-        case MEDIUM:
-            squareClass = LARGE_CLASS;
-            break;
-        case LARGE:
-            squareClass = MEDIUM_CLASS;
-            break;
-        case EXTRA_LARGE:
-            squareClass = SMALL_CLASS;
-            break;
-    }
+	let squareClass;
+	switch (gridSize) {
+		case SMALL:
+			squareClass = XL_CLASS;
+			break;
+		case MEDIUM:
+			squareClass = LARGE_CLASS;
+			break;
+		case LARGE:
+			squareClass = MEDIUM_CLASS;
+			break;
+		case EXTRA_LARGE:
+			squareClass = SMALL_CLASS;
+			break;
+	}
 
-    // clear existing squares before generating a new grid
-    sketchContainer.innerHTML = "";
+	// clear existing squares before generating a new grid
+	sketchContainer.innerHTML = "";
 
-    // generate squares
-    for (let i = 0; i < gridSize ** 2; i++) {
-        const squareElement = document.createElement("div");
-        squareElement.classList.add("square");
-        squareElement.classList.add(squareClass);
-        squareElement.setAttribute("draggable", "false");
-        sketchContainer.appendChild(squareElement);
+	// generate squares
+	for (let i = 0; i < gridSize ** 2; i++) {
+		const squareElement = document.createElement("div");
+		squareElement.classList.add("square");
+		squareElement.classList.add(squareClass);
+		squareElement.setAttribute("draggable", "false");
+		squareElement.style.borderColor = gridIsVisible ? COLORS.SQUARE_BORDER : COLORS.SQUARE_TRANSPARENT;
 
-        // event listener for painting squares
-        squareElement.addEventListener("mousedown", () => {
-            isEraseActive
-                ? eraseSquare(squareElement)
-                : paintSquare(squareElement);
-        });
-    }
+		sketchContainer.appendChild(squareElement);
+
+		const handleAction = () => {
+			isEraseActive ? eraseSquare(squareElement) : paintSquare(squareElement);
+		};
+
+		let changeColor = false;
+
+		// event listeners for painting squares
+		squareElement.addEventListener("mousedown", () => {
+			handleAction();
+			changeColor = true;
+		});
+
+		squareElement.addEventListener("mouseup", () => {
+			changeColor = false;
+		});
+
+		squareElement.addEventListener("mousemove", () => {
+			if (changeColor) {
+				handleAction();
+			}
+		});
+	}
 };
 
 btnErase.addEventListener("click", () => {
-    isEraseActive = !isEraseActive;
+	isEraseActive = !isEraseActive;
 });
 
 // Event listeners for grid size buttons
@@ -113,49 +128,44 @@ btnErase.addEventListener("click", () => {
 let currentActiveButton = null; // variable to keep track of the active btn to be able to remove class active for the rest of the btns
 
 optionButtons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        if (!gameIsRunning) {
-            prompts.forEach((line) => {
-                display.removeChild(line);
-            });
-            display.appendChild(sketchContainer);
-            gameIsRunning = true;
-        }
+	btn.addEventListener("click", (e) => {
+		if (!gameIsRunning) {
+			prompts.forEach((line) => {
+				display.removeChild(line);
+			});
+			display.appendChild(sketchContainer);
+			gameIsRunning = true;
+		}
 
-        const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = GRID_SIZES;
-        const btnId = e.currentTarget.id;
-        const led = e.currentTarget.firstElementChild;
+		const { SMALL, MEDIUM, LARGE, EXTRA_LARGE } = GRID_SIZES;
+		const btnId = e.currentTarget.id;
+		const led = e.currentTarget.firstElementChild;
 
-        if (currentActiveButton !== btn) {
-            if (currentActiveButton) {
-                currentActiveButton.firstElementChild.classList.remove(
-                    "active"
-                );
-            }
+		if (currentActiveButton !== btn) {
+			if (currentActiveButton) {
+				currentActiveButton.firstElementChild.classList.remove("active");
+			}
 
-            led.classList.add("active");
-            //set the clicked btn as active button
-            currentActiveButton = btn;
+			led.classList.add("active");
+			//set the clicked btn as active button
+			currentActiveButton = btn;
 
-            switch (btnId) {
-                case "s16":
-                    generateGrid(SMALL);
-                    break;
-                case "s32":
-                    generateGrid(MEDIUM);
-                    break;
-                case "s64":
-                    generateGrid(LARGE);
-                    break;
-                case "s128":
-                    generateGrid(EXTRA_LARGE);
-                    break;
-            }
-        }
-    });
+			switch (btnId) {
+				case "s16":
+					generateGrid(SMALL);
+					break;
+				case "s32":
+					generateGrid(MEDIUM);
+					break;
+				case "s64":
+					generateGrid(LARGE);
+					break;
+				case "s128":
+					generateGrid(EXTRA_LARGE);
+					break;
+			}
+		}
+	});
 });
 
 toggleGridBtn.addEventListener("click", toggleGrid);
-
-// init default grid
-// generateGrid(GRID_SIZES.SMALL);
